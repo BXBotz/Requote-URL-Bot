d07f3e9203b2bf54d90bffd9d0f0fbf1054cbd35
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-09-04 11:00:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,6 @@
 # All rights reserved by FayasNoushad
 # License -> https://github.com/FayasNoushad/Requote-URL-Bot/blob/main/LICENSE
 
+import os
 from requests.utils import requote_uri
 from pyrogram import Client, filters
