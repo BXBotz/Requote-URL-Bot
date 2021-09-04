49bef45a1aef3e391b7cbd3f6f7a0c3c5874626e
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-09-04 11:00:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,5 @@
 # Copyright permission under MIT License
 # All rights reserved by FayasNoushad
 # License -> https://github.com/FayasNoushad/Requote-URL-Bot/blob/main/LICENSE
+
+from pyrogram import Client, filters
