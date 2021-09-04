f1090ae3001603109584610a587d66d27abce15d
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-09-04 11:01:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,13 @@
 import os
 from requests.utils import requote_uri
 from pyrogram import Client, filters
+
+
+Bot = Client(
+    "Requote-URL-Bot",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
+)
+
+Bot.run()
